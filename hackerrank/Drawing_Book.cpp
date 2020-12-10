e384130fe61ed296f8dfc6af107e2bceb4a46826
정 성훈 <swa07016@naver.com>
2020-12-10 14:10:19 +0000

    hackerrank-Drawing_Book

---
+++ hackerrank/Drawing_Book.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the pageCount function below.
+ */
+int pageCount(int n, int p) {
+    /*
+     * Write your code here.
+     */
+    int result;
+    if(n%2!=0) n -= 1;
+    if(p%2!=0) p -= 1;
+    if(p > n-p) result = (n-p)/2;
+    else result = p/2;
+    return result; 
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int p;
+    cin >> p;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = pageCount(n, p);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
