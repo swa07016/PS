738b3b44c34c678da0f628753d0455db30d745b0
정 성훈 <swa07016@naver.com>
2020-12-20 07:24:01 +0000

    hackerrank-Beautiful_Days_at_the_Movies

---
+++ hackerrank/Beautiful_Days_at_the_Movies.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+int reverseNumber(int a) {
+    string str = to_string(a);
+    string tmp = "";
+    for(int i=str.size()-1; i>=0; i--) {
+        tmp += str[i];
+    }
+    return atoi(tmp.c_str());
+}
+
+// Complete the beautifulDays function below.
+int beautifulDays(int i, int j, int k) {
+    int cnt = 0;
+    for(int n=i; n<=j; n++) {
+        int diff = abs(n-reverseNumber(n));
+        if(diff%k==0) cnt++;
+    }
+    return cnt;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string ijk_temp;
+    getline(cin, ijk_temp);
+
+    vector<string> ijk = split_string(ijk_temp);
+
+    int i = stoi(ijk[0]);
+
+    int j = stoi(ijk[1]);
+
+    int k = stoi(ijk[2]);
+
+    int result = beautifulDays(i, j, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
