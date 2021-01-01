60ce05fd85fb4320975f0a821f1bc1a671a73661
정 성훈 <swa07016@naver.com>
2021-01-01 05:08:02 +0000

    baekjoon-15664

---
+++ baekjoon/15664.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int n, m;
+vector<int> arr;
+bool visited[9];
+
+void dfs(vector<int>& v, int depth) {
+    if(depth == m) {
+        for(int i=0; i<m; i++) cout << v[i] << " ";
+        cout << "\n";
+        return ;
+    }
+    bool same_level_visited[10001] = {false};
+    for(int i=0; i<n; i++) {
+        if(!same_level_visited[arr[i]] && !visited[i]) {
+            if(v.size() && v.back() > arr[i]) continue;
+            same_level_visited[arr[i]] = true;
+            v.push_back(arr[i]);
+            visited[i] = true;
+            dfs(v, depth+1);
+            v.pop_back();
+            visited[i] = false;
+        }
+    }
+}
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin >> n >> m;
+    for(int i=0; i<n; i++) {
+        int tmp;
+        cin >> tmp;
+        arr.push_back(tmp);
+    }
+    sort(arr.begin(), arr.end());
+    vector<int> v;
+    dfs(v, 0);
+    return 0;
+}
