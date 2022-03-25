98dfa2f7cdeb44a47535fca76ab5e25bf9f0e967
정 성훈 <swa07016@naver.com>
2022-03-25 13:23:43 +0000

    beakjoon-10403

---
+++ java_code/baekjoon/math/10403.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+        int A, B, C;
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextInt(); B = sc.nextInt(); C = sc.nextInt();
+        
+        System.out.printf("%d\n%d\n%d\n%d", 
+         (A+B)%C, ((A%C) + (B%C))%C, 
+         (A*B)%C, ((A%C) * (B%C))%C
+        );
+
+	}
+}
