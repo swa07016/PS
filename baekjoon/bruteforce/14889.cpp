a81b076df26735b5aae7ed1a0bb81b3bf741ec16
정 성훈 <swa07016@naver.com>
2022-04-06 09:46:35 +0000

    baekjoon-14889

---
+++ baekjoon/bruteforce/14889.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+
+using namespace std;
+int ans = INT_MAX;
+int n;
+int capability[25][25];
+
+/*
+    두 팀의 능력치의 차이를 반환
+*/
+int getCapabilityDifference(vector<int> start, vector<int> link) {
+    int start_total = 0, link_total = 0;
+    for(int i=0; i<n/2-1; i++) {
+        for(int j=i+1; j<n/2; j++) {
+             int a = start[i], b = start[j];
+             start_total += (capability[a][b] + capability[b][a] );   
+        }
+    }
+    
+    for(int i=0; i<n/2-1; i++) {
+        for(int j=i+1; j<n/2; j++) {
+             int a = link[i], b = link[j];
+             link_total += (capability[a][b] + capability[b][a] );   
+        }
+    }
+   
+
+    return abs(start_total - link_total);
+}
+
+/*
+    가능한 팀 조합들에 대해
+    능력치 차이를 갱신해줌.
+*/
+void makeTeamCombination(vector<int> selected) {
+     do {
+         vector<int> start, link;
+         for(int i=0; i<n; i++) {
+             if(selected[i]) start.push_back(i+1);
+             else link.push_back(i+1);
+         }
+         ans = min(ans, getCapabilityDifference(start, link)); 
+     } while(prev_permutation(selected.begin(), selected.end()));
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    cin >> n;
+    for(int i=1; i<=n; i++) 
+        for(int j=1; j<=n; j++) cin >> capability[i][j];
+    
+    vector<int> selected(n, 0);
+    for(int i=0; i<n/2; i++) selected[i] = 1;
+    
+
+    makeTeamCombination(selected);    
+    cout << ans;
+    
+    return 0;
+}
