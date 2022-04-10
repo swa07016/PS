c66209a038d59227374e1e7d0ba0f82bfb8642ef
SeongHoon Jeong <swa07016@naver.com>
2022-04-10 22:26:15 +0900

    baekjoon-11727

---
+++ baekjoon/dp/11727.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+int n;
+int dp[1010];
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    cin >> n;
+   
+    /*
+        dp[n] : 2*n 크기의 직사각형을 1*2, 2*1, 2*2 타일로 채우는 방법의 수
+        점화식 : dp[n] = dp[n-1] + 2*dp[n-2]
+            1. 2*(n-1) 크기의 직사각형을 채우고 2*1 1개로 채우는 방법
+               -> dp[n-1]
+            
+            2. 2*(n-2) 크기의 직사각형을 채우고 
+                1*2 2개로 채우는 방법, 2*2 1개로 채우는 방법
+               -> 2*dp[n-2]
+            
+            ※ 1과 2를 더한 것이 2*n 크기의 직사각형을 채우는 방법의 수
+    
+        초기값 : dp[1] = 1 (2*1 크기의 직사각형을 채우는 방법의 수)
+                      dp[2] = 3 (2*2 크기의 직사각형을 채우는 방법의 수)
+    */
+    dp[1] = 1;
+    dp[2] = 3;
+    for(int i = 3; i<=n; i++) dp[i] = (dp[i-1] + 2*dp[i-2])%10007;
+    
+    cout << dp[n];
+    return 0;
+}
