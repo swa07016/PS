a09e0d9784fab2b262e07f841ece38de66d94fa0
swa07016 <swa07016@naver.com>
2023-01-15 19:24:22 +0900

    baekjoon-25305

---
+++ baekjoon/sort/25305.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int n, k;
+int score[1010];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> k;
+    for(int i=0; i<n; i++) cin >> score[i];
+    sort(score, score+n);
+    cout << score[n-k];
+    return 0;
+}
