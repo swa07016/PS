3d44dfa6e4062f31e5be127835d9b548e5a01114
swa07016 <swa07016@naver.com>
2023-01-15 18:55:36 +0900

    baekjoon-18108

---
+++ baekjoon/io/18108.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int y;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> y;
+    cout << y - (2541-1998);
+    return 0;
+}
