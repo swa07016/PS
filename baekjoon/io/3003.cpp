4cd96d5bf7087032c6b6e48b53215e1ae1f3f828
swa07016 <swa07016@naver.com>
2023-01-15 19:05:27 +0900

    baekjoon-3003

--- baekjoon/io/3003.cpp
+++ baekjoon/io/3003.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 
 using namespace std;
-int piece[6] = {1, 1, 2, 2, 2, 8};
-int input;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
-    for(int i=0; i<6; i++) {
-        cin >> input;
-        cout << piece[i] - input << " ";
-    }
-
+    cout << "         ,r\'\"7" << "\n";
+    cout << "r`-_   ,\'  ,/" << "\n";
+    cout << " \\. \". L_r'" << "\n";
+    cout << "   `~\\/" << "\n";
+    cout << "      |" << "\n";
+    cout << "      |" << "\n";
+    cout << "\n";
     return 0;
 }
