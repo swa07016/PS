a72f89292fbb798ba98d2463d1c3a0753e3fd995
swa07016 <swa07016@naver.com>
2023-10-14 00:04:44 +0900

    baekjoon-13333

---
+++ baekjoon/math/13333.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int n;
+int cnt[1010];
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    cin >> n;
+    for(int i=0; i<n; i++)
+        cin >> cnt[i];
+
+
+    for(int i=0; i<=10000; i++) {
+        
+    }
+    
+    return 0;
+}
