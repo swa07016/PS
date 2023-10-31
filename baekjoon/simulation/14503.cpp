777d9a3e7822f0b9defd57956aefb0eb1da5492b
swa07016 <swa07016@naver.com>
2023-10-31 17:03:55 +0900

    beakjoon-1926

--- baekjoon/simulation/14503.cpp
+++ baekjoon/simulation/14503.cpp
@@ -1,56 +1,205 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
+#include <utility>
+
 using namespace std;
+int n, m;
+int board[51][51]; // board[y][x] : (0 : 청소되지 않은 칸), (1 : 벽), (-1 : 청소한 칸)
+int r, c, d; 
+int cy, cx, cd;
+// d : (0 : 북), (1 : 동), (2 : 남), (3 : 서)
+int ans;
+// 동, 서, 남, 북 순서대로 배열 세팅
+int dy[4] = {0, 0, 1, -1};
+int dx[4] = {1, -1, 0, 0};
 
-int N, M;
-int board[51][51];
-int dy[4] = {-1, 0, 1, 0};  // 북, 동, 남, 서 순서의 y방향 이동
-int dx[4] = {0, 1, 0, -1}; // 북, 동, 남, 서 순서의 x방향 이동
+bool isDirty(int y, int x) {
+    return board[y][x] == 0;
+}
 
-// 재귀적으로 청소를 수행하는 함수
-int solve(int y, int x, int d) {
-    int cnt = 0;
+void clean(int y, int x) {
+    board[y][x] = -1;
+}
 
-    // 현재 위치가 청소되지 않았다면 청소
-    if (board[y][x] == 0) {
-        board[y][x] = 2;
-        cnt++;
+bool safe(int y, int x) {
+    return (0 <= y && y < n) && (0 <= x && x < m);
+}
+
+bool isFourSpacesClean(int y, int x) {
+    int cleanCount = 0;
+    for(int i=0; i<4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if(safe(ny, nx) && (board[ny][nx] == -1 || board[ny][nx] == 1)) cleanCount++;
+    }
+    return cleanCount == 4;
+}
+
+pair<int, int> getOppositePosition(int y, int x, int d) {
+    int oppositeY, oppositeX;
+    if(d == 0) { // 북이라면 남쪽으로 한칸 가봐야함
+        oppositeY = y + dy[2];
+        oppositeX = x + dx[2];
+    }
+    if(d == 1) { // 동쪽이면 서쪽으로 한칸 가봐야함
+        oppositeY = y + dy[1];
+        oppositeX = x + dx[1];
+    }
+    if(d == 2) { // 남쪽이면 북쪽으로 한칸 가봐야함
+        oppositeY = y + dy[3];
+        oppositeX = x + dx[3];
+    }
+    if(d == 3) { // 서쪽이면 동쪽으로 한칸 가봐야함.
+        oppositeY = y + dy[0];
+        oppositeX = x + dx[0];
     }
 
-    // 네 방향 검사
-    for (int i = 0; i < 4; i++) {
-        d = (d - 1 + 4) % 4;  // 왼쪽 방향으로 회전
-        int ny = y + dy[d];
-        int nx = x + dx[d];
+    return make_pair(oppositeY, oppositeX);
+}
+
+pair<int, int> getForwardPosition(int y, int x, int d) {
+    int ny, nx;
+    if(d == 0) { // 북이면 북으로 한칸더 가서 봄.
+        ny = y + dy[3];
+        nx = x + dx[3];
+    }
+    if(d == 1) { // 동쪽이면 동으로 한칸 더 가서 봄.
+        ny = y + dy[0];
+        nx = x + dx[0];
+    }
+    if(d == 2) { // 남쪽이면 남으로 한 칸 더 가서봄
+        ny = y + dy[2];
+        nx = x + dx[2];
+    }
+    if(d == 3) { // 서쪽이면 서로 한 칸 더 가서봄
+        ny = y + dy[1];
+        nx = x + dx[1];
+    }
+
+    return make_pair(ny, nx);
+}
+
+bool canReverse(int y, int x, int d) {
+    pair<int, int> oppositePosition = getOppositePosition(y, x, d);
+
+    int oppositeY = oppositePosition.first;
+    int oppositeX = oppositePosition.second;
+
+    if(safe(oppositeY, oppositeX) && board[oppositeY][oppositeX] != 1) return true;
+    return false;
+}
+
+void executeReverse(int& y, int& x, int d) {
+    pair<int, int> oppositePosition = getOppositePosition(y, x, d);
+
+    int oppositeY = oppositePosition.first;
+    int oppositeX = oppositePosition.second;
+
+    y = oppositeY;
+    x = oppositeX;
+
+    return ;
+}
+
+void rotateRobot(int& d) {
+    if(d == 0) { // 북이면 서로
+        d = 3;
+    }
+    else if(d == 1) { // 동쪽이면 북으로
+        d = 0;
+    }
+    else if(d == 2) { // 남쪽이면 동으로
+        d = 1;
+    }
+    else if(d == 3) { // 서쪽이면 남으로 
+        d = 2;
+    }
+}
+
+
+bool isFrontSpaceCleaned(int y, int x, int d) {
+    int ny, nx;
+    pair<int, int> forwardPosition = getForwardPosition(y, x, d);
+    ny = forwardPosition.first;
+    nx = forwardPosition.second;
+    
+    if(safe(ny, nx) && board[ny][nx] == -1) return true;
+    return false;
+}
+
+void goForward(int& y, int& x, int d) {
+    int ny, nx;
+    pair<int, int> forwardPosition = getForwardPosition(y, x, d);
+    ny = forwardPosition.first;
+    nx = forwardPosition.second;
+    y = ny;
+    x = nx;
+    return ;
+}
+
+void solve() {
+    
+    // 현재 좌표 & 현재 보고있는 방향
+    cy = r;
+    cx = c;
+    cd = d;
+
+
+    while(true) {
+        // 현재 칸이 아직 청소되지 않은 경우 청소한다.
+        if(isDirty(cy, cx)) clean(cy, cx);
+
+        // 4칸이 청소가 다 된 경우,
+        if(isFourSpacesClean(cy, cx)) {
+            // 후진이 가능하면 후진하고 1번으로 돌아간다.
+            if(canReverse(cy, cx, cd)) {
+                executeReverse(cy, cx, cd);
+                continue;
+            }
+
+            // 후진이 불가능하다면 멈춘다. (종료조건)
+            else break;
+        }
+            
         
-        // 회전한 방향에 청소하지 않은 칸이 있다면 그 방향으로 이동 후 청소 수행
-        if (0 <= ny && ny < N && 0 <= nx && nx < M && board[ny][nx] == 0) {
-            cnt += solve(ny, nx, d);
-            return cnt;
+        // 4칸 중 청소되지 않은 빈칸이 있는 경우
+        else {
+            // 반시계 방향으로 90도 회전한다. 
+            rotateRobot(cd);
+            // 바라보는 방향을 기준으로 앞쪽 칸이 청소되지 않은 빈 칸인 경우 한 칸 전진한다.
+            if(!isFrontSpaceCleaned(cy, cx, cd)) {
+                goForward(cy, cx, cd);
+                // 1번으로 돌아간다.
+                continue;
+            }
         }
     }
+        
+}
 
-    // 모든 방향이 청소되어있거나 벽인 경우
-    int ny = y - dy[d];  // 뒤쪽 방향으로 이동
-    int nx = x - dx[d];
-    if (0 <= ny && ny < N && 0 <= nx && nx < M && board[ny][nx] != 1) {  // 뒤쪽 방향이 벽이 아니면 이동
-        return cnt + solve(ny, nx, d);
+void countClean() {
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++)
+        if(board[i][j] == -1) ans++;
     }
-    return cnt;
 }
 
 int main() {
-    cin >> N >> M;  // 방의 크기 입력
-    int r, c, d;  // 로봇의 초기 위치와 방향 입력
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    cin >> n >> m;
     cin >> r >> c >> d;
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            cin >> board[i][j];  // 방의 상태 입력
-        }
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++)
+            cin >> board[i][j];
     }
 
-    // 청소 시작 및 결과 출력
-    cout << solve(r, c, d) << '\n';
+    solve();
+    countClean();
+    cout << ans;
+
     return 0;
 }
