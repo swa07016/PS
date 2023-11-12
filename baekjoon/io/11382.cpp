b560e058865f2bf271bbfc31c06fb9be5a82d46f
swa07016 <swa07016@naver.com>
2023-11-12 20:19:19 +0900

    baekjoon-11382

---
+++ baekjoon/io/11382.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    // long long 자료형을 사용하여 큰 수를 처리할 수 있도록 합니다.
+    long long A, B, C;
+
+    // A, B, C 값을 입력받습니다.
+    std::cin >> A >> B >> C;
+
+    // A, B, C의 합을 계산하고 출력합니다.
+    std::cout << A + B + C << std::endl;
+
+    return 0;
+}
