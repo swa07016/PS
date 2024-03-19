7db917879ebab8f3613d850ff944b6b033391c0e
swa07016 <swa07016@naver.com>
2024-03-19 21:16:39 +0900

    baekjoon-7662

---
+++ baekjoon/data-structure/7662.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<set>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t, k, n;
+	char ins;
+	cin >> t;
+	while (t--) {
+		multiset<int> ms;
+		cin >> k;
+		while (k--) {
+			cin >> ins >> n;
+			if (ins == 'I') {
+				ms.insert(n);
+			}
+			else {
+				if (ms.empty()) continue;
+
+				if (n == 1) {
+					ms.erase(--ms.end());
+				}
+				else {
+					ms.erase(ms.begin());
+				}
+			}
+		}
+
+		if (ms.empty()) cout << "EMPTY\n";
+		else {
+			cout << *(--ms.end()) << " " << *ms.begin() << "\n";
+		}
+	}
+
+	return 0;
+}
